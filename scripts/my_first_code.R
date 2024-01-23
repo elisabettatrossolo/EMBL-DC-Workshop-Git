735477cfd0bd9ef517cc17b6427d3db8d5c806f8
Trossolo Elisabetta (Student AGR 19) <etrossolo@unibz.it>
2024-01-23 15:01:43 +0100

    this is my first R script

---
+++ scripts/my_first_code.R
@@ -0,0 +1,29 @@
+3 + 4 
+
+weight_kg <- 55
+
+mickey <- 27
+
+
+
+
+
+
+
+
+
+  
+Weight_kg <- 34
+
+my_weight_kg <- 50
+(my_weight <- 50)
+my_weight
+
+2.2 * weight_kg
+
+weight_kg <- 57
+2.2 * weight_kg
+
+weight_lb <- 2.2*weight_kg
+weight_kg <- 100
+
