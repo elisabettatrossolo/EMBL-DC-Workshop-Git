17b8f870cc789450d49793719009fd6b3984a968
Trossolo Elisabetta (Student AGR 19) <etrossolo@unibz.it>
2024-01-24 18:25:09 +0100

    GGPlot

--- scripts/my_tird_code.R
+++ scripts/my_tird_code.R
@@ -50,6 +50,60 @@ surveys[nrow(surveys)/2, ]
 my_list <- list(name = c("Nora", "Lisanna", "Francesco"), money = c(1,6,7,3,5,8))
 
 my_list[[1]]
-my_list&names
+my_list$names
 
 surveys[[3]]
+
+### Factors R store factor in a nice way  
+str(surveys)
+surveys$sex <- factor(surveys$sex) # dollaro seleziona colonna
+
+levels(surveys$sex)
+nlevels(surveys$sex)
+
+sex <- factor(c("male", "female", "female", "male"))
+sex <- factor(sex, levels = c("male", "female"))
+
+### exercise 
+
+surveys$taxa <- factor(surveys$taxa)
+surveys$genus <- factor(surveys$genus)
+
+levels(surveys$taxa)
+
+nlevels(surveys$genus) # da il numero dei generi
+
+sum(surveys$taxa == "Rabbit") # cosi conti il numero dei conigli 
+
+summary(surveys) # another way ti da il numero di tutti i taxa
+
+#convert factivor in character
+as.character(sex)
+
+year_fct <- factor(c(1990, 1983, 1977, 1997))
+as.numeric(year_fct)
+
+as.numeric(as.character(year_fct))
+as.numeric(levels(year_fct))[year_fct] 
+
+#renamig factors
+plot(surveys$sex)
+
+summary(surveys$sex)
+sex <- surveys$sex
+levels(sex)
+sex<- addNA(sex)
+levels(sex)
+
+levels(sex)[1] <- "undertermined"
+levels(sex)
+sex
+
+plot(sex)
+
+levels(sex)[1:2] <-c("female", "male") 
+plot(sex)
+
+sex <- factor(sex,levels = c("undertermined","female","male"))
+plot(sex)
+              
