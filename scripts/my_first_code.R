83a59faac27db9a953b3213745d1df1570f37393
Trossolo Elisabetta (Student AGR 19) <etrossolo@unibz.it>
2024-01-24 10:07:09 +0100

    Last

--- scripts/my_first_code.R
+++ scripts/my_first_code.R
@@ -28,3 +28,28 @@ weight_lb <- 2.2*weight_kg
 # I have to update the value of the weight_kg object
 weight_kg <- 100
 
+
+weight_lb
+
+###Exercise###
+
+mass <- 47.5
+age <- 122
+
+mass <- mass*2
+age <- age-20
+mass_index <- mass/age
+
+weigh_kg <- sqrt(9) 
+
+round(3.14159)
+round (3.15159, digits = 2)
+round (3.14159, 2)
+
+round(digits = 2, x = 3.14159)
+round(2, 3.14159)
+round(digits =2)
+
+
+
+
